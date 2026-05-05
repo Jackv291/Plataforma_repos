8fd0a468d57f5445c6b72b1c58427a7148eb1e93
Jackv291 <jack.jizhou.23@campusfp.es>
2026-05-05 21:39:26 +0200

    Dash

--- Assets/Scripts/Movimiento.cs
+++ Assets/Scripts/Movimiento.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -8,34 +9,93 @@ public class Movimiento : MonoBehaviour
 {
     public float velocidad = 6.7f;
     public float salto = 6.7f;
-    private float direccion = 0;
-    private Rigidbody jugador;
+    public float gravedad = -9.81f;
+    public float dash = 30f;
+    public float dashDuration = 0.15f;
+    public float cooldowndash = 0f;
+
+    public CharacterController controller;
+    private Vector3 playerVelocity;
+    private Vector3 dashVelocity = Vector3.zero;
+    private bool groundedPlayer;
+    private bool DashSi = true;
     // Start is called before the first frame update
+    private int ultimadireccion = 1;
     void Start()
     {
-        jugador = GetComponent<Rigidbody>();
-        transform.position = new Vector3(0, 0, 0);
-    }
+        controller = GetComponent<CharacterController>();
 
+    }
     // Update is called once per frame
     void Update()
     {
-        direccion = Input.GetAxis("Horizontal");
-
-        if (direccion > 0)
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer)
         {
-            jugador.velocity = new Vector3(velocidad, jugador.velocity.y);
-        }
-        else if (direccion < 0)
-        {
-            jugador.velocity = new Vector3(-velocidad, jugador.velocity.y);
-        }
-        {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-                jugador.velocity = new Vector3(0, salto , 0 );
+            // Slight downward velocity to keep grounded stable
+            if (playerVelocity.y < 0f)
+                playerVelocity.y = -2f;
         }
 
+        // Read input
+        float valorX = Input.GetAxis("Horizontal");
+        Vector3 move = new Vector3(valorX, 0, 0);
+        move = Vector3.ClampMagnitude(move, 1f);
+
+        if (valorX > 0)
+            ultimadireccion = 1;
+        else if (valorX < 0)
+            ultimadireccion = -1;
+
+        if (move != Vector3.zero)
+            transform.forward = move;
+
+        // Jump using WasPressedThisFrame()
+        if (groundedPlayer && Input.GetKeyDown(KeyCode.Space))
+        {
+            playerVelocity.y = salto * -gravedad;
         }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && DashSi)
+        {
+            int direccion;
+            if (valorX > 0)
+                direccion = 1;
+            else if (valorX < 0)
+                direccion = -1;
+            else
+                direccion = ultimadireccion;
+
+            StartCoroutine(HazDash(direccion));
+        }
+
+        if (cooldowndash > 0f)
+        {
+            cooldowndash -= Time.deltaTime;
+            if (cooldowndash <= 0f)
+            {
+                dashVelocity = Vector3.zero;
+                cooldowndash = 0f;
+            }
+        }
+
+        // Apply gravity
+        playerVelocity.y += gravedad * Time.deltaTime;
+
+        // Move
+        Vector3 finalMove = move * velocidad + dashVelocity + Vector3.up * playerVelocity.y;
+        controller.Move(finalMove * Time.deltaTime);
+    }
+    private IEnumerator HazDash(int direccion)
+    {   DashSi = false;
+        dashVelocity = Vector3.right * direccion * dash;
+
+        yield return new WaitForSeconds(dashDuration);
+        dashVelocity = Vector3.zero;
+
+        cooldowndash = 1f; // Cooldown of 1 second
+        yield return new WaitForSeconds(cooldowndash);
+
+        DashSi = true;
     }
 }
